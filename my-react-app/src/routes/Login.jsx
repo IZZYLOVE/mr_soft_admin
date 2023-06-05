cc6381d71cc0a5a51516b9f08c04006983ad8727
judeDave <judedavid688@gmail.com>
2023-06-05 13:52:57 +0100

    form functionality update

--- my-react-app/src/routes/Login.jsx
+++ my-react-app/src/routes/Login.jsx
@@ -1,37 +1,110 @@
-import './login.css'
-import {Link} from "react-router-dom"
+import './login.css';
+import { Link } from 'react-router-dom';
 import { Icon } from '@iconify/react';
-
+import React, { useState } from 'react';
+import { BeatLoader } from 'react-spinners';
 
 export function Login() {
-    return < div className='body'>
-    <div class="wrapper">
-    <Link to='/'><Icon icon="#" id='loginicon' width='30px'/>...</Link>
-        <div class="form">
-            <h2>Login</h2>
-            <form action="#">
-                
-        <div class="input-box">
-            <input type="email" name="email" required />  
-            <label>Email</label>   
-        </div>
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState('');
 
-        <div class="input-box">
-            <input type="password" name="password" required />  
-            <label>Password</label>   
-        </div>
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-        <div class="remember">
-            <label><input type="checkbox"/>
-            Remember me</label>
-            <Link to='#'>Forgot password</Link>
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('api/v1/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const { token } = await response.json();
+        localStorage.setItem('token', token);
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        alert('Form submitted successfully');
+      } else {
+        const { error } = await response.json();
+        setError(error);
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      setError('Incorrect credentials...');
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <div className='body'>
+      <div className='wrapper'>
+        <Link to='/'>
+          <Icon icon='#' id='loginicon' width='30px' />
+          ...
+        </Link>
+        <div className='form'>
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className='input-box'>
+              <input
+                type='email'
+                name='email'
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+              <label>Email</label>
+            </div>
+
+            <div className='input-box'>
+              <input
+                type='password'
+                name='password'
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+              <label>Password</label>
+            </div>
+
+            <div className='remember'>
+              <label>
+                <input type='checkbox' />
+                Remember me
+              </label>
+              <Link to='#'>Forgot password</Link>
+            </div>
+
+            {error && <div className='error-message'>{error}</div>}
+
+            <button type='submit' className='butn' disabled={isLoading} required>
+              {isLoading ? (
+                <BeatLoader color='#ffffff' loading={isLoading} size={8} />
+              ) : (
+                'Submit'
+              )}
+            </button>
+
+            <div className='no-account'>
+              Don't have an account? <Link to='/register'>Register</Link>
+            </div>
+          </form>
         </div>
-        <button type="submit" class="butn">Login
-        </button>
-         <div className='no-account'>Dont have an account? <Link to='/register' className='reglink'>Register</Link></div>
-            </form>
-        </div>
+      </div>
     </div>
-    
-    </div>
+  );
 }
--- my-react-app/src/routes/Register.jsx
+++ my-react-app/src/routes/Register.jsx
@@ -1,139 +1,187 @@
 import './register.css'
 import React, { useState } from 'react';
+import { BeatLoader } from 'react-spinners';
 
-const Rooturl =  'http://127.0.0.1:7300/'
+const Rooturl = 'http://127.0.0.1:7300/';
 
 export function Register() {
-    const [formData, setFormData] = useState({
-        firstName: '',
-        middleName:'',
-        lastName:'',
-        email: '',
-        phone: '',
-        password:'',
-        confirmPassword:'',
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: '',
+    middleName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    password: '',
+    confirmPassword: '',
+    gender: '', // New gender field
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const validateForm = () => {
+    // Perform basic form validation
+    const { firstName, lastName, email, phone, password, confirmPassword, gender } = formData;
+    if (!firstName || !lastName || !email || !phone || !password || !confirmPassword || !gender) {
+      alert('Please fill in all required fields.');
+      return false;
+    }
+    if (password !== confirmPassword) {
+      alert('Passwords do not match.');
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    if (!validateForm()) {
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const response = await fetch(`${Rooturl}api/v1/users/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
       });
 
-      const handleChange = (event) => {
-        setFormData({
-          ...formData,
-          [event.target.name]: event.target.value,
-        });
-      };
+      const data = await response.json();
+      console.log(data);
+      console.log(data.token);
+      // Handle the response from the backend if needed
 
-      const handleSubmit = (event) => {
-        event.preventDefault();
-        let fdx = JSON.stringify(formData)
-        console.log(fdx)
-        fetch(`${Rooturl}api/v1/users/signup`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData)
-          
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            // Handle the response from the backend if needed
-            console.log(data)
-          })
-          .catch((error) => {
-            // Handle any errors
-          });
-      };
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      alert('Form submitted successfully');
+    } catch (error) {
+      // Handle any errors
+      console.error('Form submission error:', error);
+    }
+    setIsLoading(false);
+  };
 
+  return (
+    <div className='regbody'>
+      <div className='regwrapper'>
+        <div className='regform'>
+          <h2>Register</h2>
+          <form onSubmit={handleSubmit}>
+            <div className='firstrow'>
+              <div className='input-box'>
+                <input
+                  type='text'
+                  name='firstName'
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  required
+                />
+                <label>First-name*</label>
+              </div>
 
+              <div className='input-box'>
+                <input
+                  type='text'
+                  name='middleName'
+                  value={formData.middleName}
+                  onChange={handleChange}
+                  required
+                />
+                <label>Middlename-name*</label>
+              </div>
 
-    return <div className='regbody'>
-     <div class="regwrapper">
-        <div class="regform">
-            <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
+              <div className='input-box'>
+                <input
+                  type='text'
+                  name='lastName'
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  required
+                />
+                <label>Last-name*</label>
+              </div>
+            </div>
 
-<div className='firstrow'>
+            <div className='secondrow'>
+              <div className='input-box'>
+                <input
+                  type='text'
+                  name='email'
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+                <label>Email*</label>
+              </div>
 
-      <div class="input-box">
-            <input type="text" 
-            name="firstName" 
-            value={formData.firstname}
-            onChange={handleChange}   
-            required />  
-            <label>First-name*</label>
-           
-       </div>
-        
-       <div class="input-box">
-            <input type="text" 
-            name="middleName" 
-            value={formData.middlename}
-            onChange={handleChange}   
-            required />  
-            <label>Middlename-name*</label>   
-       </div>
+              <div className='input-box'>
+                <input
+                  type='number'
+                  name='phone'
+                  value={formData.phone}
+                  onChange={handleChange}
+                  required
+                />
+                <label>Phone*</label>
+              </div>
+            </div>
 
-       <div class="input-box">
-            <input type="text" 
-            name="lastName" 
-            value={formData.lastname}
-            onChange={handleChange}   
-            required />  
-            <label>Last-name*</label>   
-       </div>
-</div>
+            <div className='thirdrow'>
+              <div className='input-box'>
+                <input
+                  type='password'
+                  name='password'
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+                <label>Password*</label>
+              </div>
 
-<div className='secondrow'>  
-        <div class="input-box">
-            <input type="text" 
-            name="email"
-            value={formData.email}
-            onChange={handleChange}    
-            required />  
-            <label>Email*</label>   
+              <div className='input-box'>
+                <input
+                  type='password'
+                  name='confirmPassword'
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  required
+                />
+                <label>Confirm-password*</label>
+              </div>
+            </div>
+
+            <div className='select-input-box'>
+              <select
+                name='gender'
+                value={formData.gender}
+                onChange={handleChange}
+                required
+              >
+                <option value=''>Select Gender</option>
+                <option value='Male'>Male</option>
+                <option value='Female'>Female</option>
+              </select>
+            </div>
+
+            <button type='submit' className='butn' disabled={isLoading} required>
+              {isLoading ? (
+                <BeatLoader color='#ffffff' loading={isLoading} size={8} />
+              ) : (
+                'Submit'
+              )}
+            </button>
+          </form>
         </div>
-
-        <div class="input-box">
-            <input type="number" 
-            name="phone" 
-            value={formData.phone}
-            onChange={handleChange}   
-            required />  
-            <label>Phone*</label>
-        </div>
-
-</div> 
-
-<div className='thirdrow'>
-        <div class="input-box">
-            <input type="password" 
-            name="password" 
-            value={formData.password}
-            onChange={handleChange}   
-            required />  
-            <label>Password*</label>   
-        </div>
-
-        <div class="input-box">
-            <input type="password" 
-            name="confirmPassword" 
-            value={formData.confirmpassword}
-            onChange={handleChange}   
-            required />  
-            <label>Confirm-password*</label>   
-       </div>
-</div> 
-        <div class="remember">
-            <label></label>
-           
-        </div>
-
-        <button type="submit" class="butn" required>Register
-        </button>
-
-        
-            </form>
-        </div>
+      </div>
     </div>
-    
-    </div>
+  );
 }
