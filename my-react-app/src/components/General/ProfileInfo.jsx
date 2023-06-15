411e69a22fcc5665e7b832d31354ba5e57510124
IZZYLOVE <izzylovu@gmail.com>
2023-06-16 00:45:54 +0100

    updated user profile

--- my-react-app/src/components/General/ProfileInfo.jsx
+++ my-react-app/src/components/General/ProfileInfo.jsx
@@ -11,22 +11,32 @@ export function ProfileInfo() {
     return () => {};
   }, [setPageTitle]);
 
+  let address = getStoredUserObj().address
+
+  if(!address){
+    address = "PLEASE UPDATE YOUR PROFILE WITH AN ADDRESS"
+  }
   return (
     <div className="myheader">
-      <div className="profile">
-        {/* Icon component */}
-        <div className="profilesidebar">
-          <Icon icon="mdi:user-circle" width="200" alt="Icon" className="profileimg" />
-        </div>
 
-        {/* Profile information component */}
-        <div className="profilemain">
-          <p><b>Name:</b>  <span>{`${getStoredUserObj().firstName.toUpperCase()} ${getStoredUserObj().middleName.toUpperCase()} ${getStoredUserObj().lastName.toUpperCase()}`}</span></p>
-          <p><b>Role:</b>  <span>{`${getStoredUserObj().role.toUpperCase()}`}</span></p>
-          <p><b>Email:</b> <span>{`${getStoredUserObj().email.toUpperCase()}`}</span></p>
-          <p><b>Phone:</b> <span>{`${getStoredUserObj().phone.toUpperCase()}`}</span></p>
+      
+		<div class="just_a_container">
+		<div class="main_flex_container padme">
+				<div class="sonx son1x">
+        <Icon icon="mdi:user-circle" width="320" alt="Icon" className="profileimg" />
         </div>
-      </div>
-    </div>
+				<div class="sonx son2x" id='Overideflexdirection1'>
+            <p ><b>Name:</b>  <span>{`${getStoredUserObj().firstName.toUpperCase()} ${getStoredUserObj().middleName.toUpperCase()} ${getStoredUserObj().lastName.toUpperCase()}`}</span></p>
+            <p><b>Role:</b>  <span>{`${getStoredUserObj().role.toUpperCase()}`}</span></p>
+            <p><b>Email:</b> <span>{`${getStoredUserObj().email.toUpperCase()}`}</span></p>
+            <p><b>Phone:</b> <span>{`${getStoredUserObj().phone.toUpperCase()}`}</span></p>
+        </div>
+				<div class="sonx son3x" id='Overideflexdirection2'>
+        <p><b>Adress:</b> <span>{`${address.toUpperCase()}`}</span></p>
+        </div>
+		</div>
+		</div>
+    
+	</div>
   );
 }
